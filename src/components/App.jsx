fe162923633070ee2068bf4ec7df3847790ecc3f
Marini23 <marina.smile1m@gmail.com>
2024-01-23 18:15:43 +0100

    create-routers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Routes>
+        <Route path="/" element={<div>Layout</div>}>
+          <Route index element={<div>HomePage</div>} />
+          <Route path="/register" element={<div>Register</div>} />
+          <Route path="/login" element={<div>Log In</div>} />
+          <Route path="/recipies" element={<div>Recipies List</div>} />
+          <Route path="/favorites" element={<div>Favorites</div>} />
+          <Route path="/shoppinglist" element={<div>Shopping List</div>} />
+          <Route path="*" element={<div>Homepage</div>} />
+        </Route>
+      </Routes>
     </div>
   );
 };
